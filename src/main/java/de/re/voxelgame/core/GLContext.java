039441d1527f1a09dc0556d47432b1d77be2c04c
Raffael E <raffa_e@web.de>
2021-11-23 21:48:09 +0100

    Abstraction for initializing & managing glfw

---
+++ src/main/java/de/re/voxelgame/core/GLContext.java
@@ -0,0 +1,97 @@
+package de.re.voxelgame.core;
+
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.system.MemoryStack;
+
+import java.nio.IntBuffer;
+
+import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.glfw.GLFW.glfwShowWindow;
+import static org.lwjgl.opengl.GL.createCapabilities;
+
+public class GLContext {
+  public static final long NULL = 0L;
+
+  private final long window;
+
+  public GLContext(int width, int height, String title) {
+    // Setup error callback. Default implementation will print error messages in System.err
+    GLFWErrorCallback.createPrint(System.err).set();
+
+    // Init GLFW
+    if (!glfwInit()) {
+      throw new IllegalStateException("Unable to initialize GLFW!");
+    }
+
+    // Configure GLFW
+    glfwDefaultWindowHints(); // Optional, current hints are already the defaults
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+
+    // Create window
+    window = glfwCreateWindow(width, height, title, NULL, NULL);
+    if (window == NULL) {
+      glfwTerminate();
+      throw new IllegalStateException("Failed to create GLFW window!");
+    }
+
+    // Setup key callback
+    glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
+      if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE) {
+        glfwSetWindowShouldClose(window, true);
+      }
+    });
+
+    // Get thread stack & push a new frame
+    try (MemoryStack stack = MemoryStack.stackPush()) {
+      IntBuffer pWidth = stack.mallocInt(1); // int pointer
+      IntBuffer pHeight = stack.mallocInt(1); // int pointer
+
+      glfwGetWindowSize(window, pWidth, pHeight);
+      GLFWVidMode vidMode = glfwGetVideoMode(glfwGetPrimaryMonitor()); // Get resolution of primary monitor
+
+      // Center window
+      glfwSetWindowPos(
+              window,
+              (vidMode.width() - pWidth.get(0)) / 2,
+              (vidMode.height() - pHeight.get(0)) / 2);
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1); // 1: V-Sync, 0: Remove fps cap
+    glfwShowWindow(window);
+
+    // This line is critical for LWJGL's interoperation with GLFW's
+    // OpenGL context, or any context that is managed externally.
+    // LWJGL detects the context that is current in the current thread,
+    // creates the GLCapabilities instance and makes the OpenGL
+    // bindings available for use.
+    createCapabilities();
+  }
+
+  public void update() {
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  public boolean isCloseRequested() {
+    return glfwWindowShouldClose(window);
+  }
+
+  public void terminate() {
+    glfwFreeCallbacks(window);
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+    glfwSetErrorCallback(null).free(); // Free error callback
+  }
+
+  public long getWindow() {
+    return window;
+  }
+}
--- src/main/java/de/re/voxelgame/core/HelloWorld.java
+++ src/main/java/de/re/voxelgame/core/HelloWorld.java
@@ -1,15 +1,8 @@
 package de.re.voxelgame.core;
 
 import org.lwjgl.Version;
-import org.lwjgl.glfw.GLFWErrorCallback;
-import org.lwjgl.glfw.GLFWVidMode;
-import org.lwjgl.system.MemoryStack;
 
-import java.nio.IntBuffer;
-
-import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
 import static org.lwjgl.glfw.GLFW.*;
-import static org.lwjgl.opengl.GL.createCapabilities;
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.opengl.GL15.*;
 import static org.lwjgl.opengl.GL20.*;
@@ -17,10 +10,6 @@ import static org.lwjgl.opengl.GL30.glBindVertexArray;
 import static org.lwjgl.opengl.GL30.glGenVertexArrays;
 
 public class HelloWorld {
-  private static final long NULL = 0L;
-
-  private long window;
-
   public static void main(String[] args) {
     new HelloWorld().run();
   }
@@ -28,71 +17,13 @@ public class HelloWorld {
   public void run() {
     System.out.println("LWJGL " + Version.getVersion());
 
-    init();
-    loop();
+    GLContext context = new GLContext(1080, 720, "OpenGL");
+    loop(context);
 
-    glfwFreeCallbacks(window);
-    glfwDestroyWindow(window);
-
-    glfwTerminate();
-    glfwSetErrorCallback(null).free(); // Free error callback
+    context.terminate();
   }
 
-  private void init() {
-    // Setup error callback. Default implementation will print error messages in System.err
-    GLFWErrorCallback.createPrint(System.err).set();
-
-    // Init GLFW
-    if (!glfwInit()) {
-      throw new IllegalStateException("Unable to initialize GLFW!");
-    }
-
-    // Configure GLFW
-    glfwDefaultWindowHints(); // Optional, current hints are already the defaults
-    glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // Window will stay hidden after creation
-    glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
-
-    // Create window
-    window = glfwCreateWindow(1080, 720, "Hello World", NULL, NULL);
-    if (window == NULL) {
-      throw new IllegalStateException("Failed to create GLFW window!");
-    }
-
-    // Setup key callback
-    glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
-      if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE) {
-        glfwSetWindowShouldClose(window, true);
-      }
-    });
-
-    // Get thread stack & push a new frame
-    try (MemoryStack stack = MemoryStack.stackPush()) {
-      IntBuffer pWidth = stack.mallocInt(1); // int pointer
-      IntBuffer pHeight = stack.mallocInt(1); // int pointer
-
-      glfwGetWindowSize(window, pWidth, pHeight);
-      GLFWVidMode vidMode = glfwGetVideoMode(glfwGetPrimaryMonitor()); // Get resolution of primary monitor
-
-      // Center window
-      glfwSetWindowPos(
-              window,
-              (vidMode.width() - pWidth.get(0)) / 2,
-              (vidMode.height() - pHeight.get(0)) / 2);
-    }
-
-    glfwMakeContextCurrent(window);
-    glfwSwapInterval(1); // V-Sync
-    glfwShowWindow(window);
-  }
-
-  private void loop() {
-    // This line is critical for LWJGL's interoperation with GLFW's
-    // OpenGL context, or any context that is managed externally.
-    // LWJGL detects the context that is current in the current thread,
-    // creates the GLCapabilities instance and makes the OpenGL
-    // bindings available for use.
-    createCapabilities();
-
+  private void loop(GLContext context) {
     // Geometry
     float[] vertices = {
             -0.5f, -0.5f, 0.0f,
@@ -128,7 +59,7 @@ public class HelloWorld {
 
     Shader shader = new Shader(vert, frag);
 
-    while (!glfwWindowShouldClose(window)) {
+    while (!context.isCloseRequested()) {
       glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
       glClear(GL_COLOR_BUFFER_BIT);
 
@@ -138,8 +69,7 @@ public class HelloWorld {
       glDrawArrays(GL_TRIANGLES, 0, 3);
       glBindVertexArray(0);
 
-      glfwSwapBuffers(window);
-      glfwPollEvents();
+      context.update();
     }
   }
 }
