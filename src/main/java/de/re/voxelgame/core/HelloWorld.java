10cb1837896b291e1ab34df66f16b9da84846cd9
Raffael E <raffa_e@web.de>
2021-12-04 21:22:03 +0100

    Render texture, adjust vertices

--- src/main/java/de/re/voxelgame/core/HelloWorld.java
+++ src/main/java/de/re/voxelgame/core/HelloWorld.java
@@ -1,18 +1,19 @@
 package de.re.voxelgame.core;
 
+import de.matthiasmann.twl.utils.PNGDecoder;
 import org.joml.Matrix4f;
 import org.joml.Vector3f;
 import org.lwjgl.Version;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.nio.ByteBuffer;
 
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.opengl.GL15.*;
 import static org.lwjgl.opengl.GL20.*;
-import static org.lwjgl.opengl.GL30.glBindVertexArray;
-import static org.lwjgl.opengl.GL30.glGenVertexArrays;
+import static org.lwjgl.opengl.GL30.*;
 
 public class HelloWorld {
   public static void main(String[] args) throws IOException, URISyntaxException {
@@ -32,52 +33,52 @@ public class HelloWorld {
     // Geometry
     float[] vertices = {
             // Front
-            -1.0f,  1.0f, 1.0f, 0.0f, 1.0f,
-            -1.0f, -1.0f, 1.0f, 0.0f, 0.0f,
-             1.0f, -1.0f, 1.0f, 1.0f, 0.0f,
-             1.0f, -1.0f, 1.0f, 1.0f, 0.0f,
-             1.0f,  1.0f, 1.0f, 1.0f, 1.0f,
-            -1.0f,  1.0f, 1.0f, 0.0f, 1.0f,
+            -1.0f,  1.0f, 1.0f, 1.0f, 0.0f,
+            -1.0f, -1.0f, 1.0f, 1.0f, 1.0f,
+             1.0f, -1.0f, 1.0f, 0.0f, 1.0f,
+             1.0f, -1.0f, 1.0f, 0.0f, 1.0f,
+             1.0f,  1.0f, 1.0f, 0.0f, 0.0f,
+            -1.0f,  1.0f, 1.0f, 1.0f, 0.0f,
 
             // Back
-             1.0f,  1.0f, -1.0f, 0.0f, 1.0f,
-             1.0f, -1.0f, -1.0f, 0.0f, 0.0f,
-            -1.0f, -1.0f, -1.0f, 1.0f, 0.0f,
-            -1.0f, -1.0f, -1.0f, 1.0f, 0.0f,
-            -1.0f,  1.0f, -1.0f, 1.0f, 1.0f,
-             1.0f,  1.0f, -1.0f, 0.0f, 1.0f,
+             1.0f,  1.0f, -1.0f, 1.0f, 0.0f,
+             1.0f, -1.0f, -1.0f, 1.0f, 1.0f,
+            -1.0f, -1.0f, -1.0f, 0.0f, 1.0f,
+            -1.0f, -1.0f, -1.0f, 0.0f, 1.0f,
+            -1.0f,  1.0f, -1.0f, 0.0f, 0.0f,
+             1.0f,  1.0f, -1.0f, 1.0f, 0.0f,
 
             // Left
-            -1.0f,  1.0f,  1.0f, 0.0f, 1.0f,
-            -1.0f,  1.0f, -1.0f, 0.0f, 0.0f,
-            -1.0f, -1.0f, -1.0f, 1.0f, 0.0f,
-            -1.0f, -1.0f, -1.0f, 1.0f, 0.0f,
-            -1.0f, -1.0f,  1.0f, 1.0f, 1.0f,
-            -1.0f,  1.0f,  1.0f, 0.0f, 1.0f,
+            -1.0f,  1.0f, -1.0f, 1.0f, 0.0f,
+            -1.0f, -1.0f, -1.0f, 1.0f, 1.0f,
+            -1.0f, -1.0f,  1.0f, 0.0f, 1.0f,
+            -1.0f, -1.0f,  1.0f, 0.0f, 1.0f,
+            -1.0f,  1.0f,  1.0f, 0.0f, 0.0f,
+            -1.0f,  1.0f, -1.0f, 1.0f, 0.0f,
 
             // Right
-            1.0f, -1.0f,  1.0f, 0.0f, 1.0f,
-            1.0f, -1.0f, -1.0f, 0.0f, 0.0f,
-            1.0f,  1.0f, -1.0f, 1.0f, 0.0f,
-            1.0f,  1.0f, -1.0f, 1.0f, 0.0f,
-            1.0f,  1.0f,  1.0f, 1.0f, 1.0f,
-            1.0f, -1.0f,  1.0f, 0.0f, 1.0f,
+            1.0f,  1.0f,  1.0f, 1.0f, 0.0f,
+            1.0f, -1.0f,  1.0f, 1.0f, 1.0f,
+            1.0f, -1.0f, -1.0f, 0.0f, 1.0f,
+            1.0f, -1.0f, -1.0f, 0.0f, 1.0f,
+            1.0f,  1.0f, -1.0f, 0.0f, 0.0f,
+            1.0f,  1.0f,  1.0f, 1.0f, 0.0f,
 
             // Top
-             1.0f,  1.0f,  1.0f, 0.0f, 1.0f,
-             1.0f,  1.0f, -1.0f, 0.0f, 0.0f,
-            -1.0f,  1.0f, -1.0f, 1.0f, 0.0f,
-            -1.0f,  1.0f, -1.0f, 1.0f, 0.0f,
-            -1.0f,  1.0f,  1.0f, 1.0f, 1.0f,
-             1.0f,  1.0f,  1.0f, 0.0f, 1.0f,
+             1.0f,  1.0f,  1.0f, 1.0f, 0.0f,
+             1.0f,  1.0f, -1.0f, 1.0f, 1.0f,
+            -1.0f,  1.0f, -1.0f, 0.0f, 1.0f,
+            -1.0f,  1.0f, -1.0f, 0.0f, 1.0f,
+            -1.0f,  1.0f,  1.0f, 0.0f, 0.0f,
+             1.0f,  1.0f,  1.0f, 1.0f, 0.0f,
 
             // Bottom
-            -1.0f, -1.0f,  1.0f, 0.0f, 1.0f,
-            -1.0f, -1.0f, -1.0f, 0.0f, 0.0f,
-             1.0f, -1.0f, -1.0f, 1.0f, 0.0f,
-             1.0f, -1.0f, -1.0f, 1.0f, 0.0f,
-             1.0f, -1.0f,  1.0f, 1.0f, 1.0f,
-            -1.0f, -1.0f,  1.0f, 0.0f, 1.0f
+            -1.0f, -1.0f,  1.0f, 1.0f, 0.0f,
+            -1.0f, -1.0f, -1.0f, 1.0f, 1.0f,
+             1.0f, -1.0f, -1.0f, 0.0f, 1.0f,
+             1.0f, -1.0f, -1.0f, 0.0f, 1.0f,
+             1.0f, -1.0f,  1.0f, 0.0f, 0.0f,
+            -1.0f, -1.0f,  1.0f, 1.0f, 0.0f
     };
 
     int vao = glGenVertexArrays();
@@ -99,6 +100,24 @@ public class HelloWorld {
     ResourceLoader.Resource frag = ResourceLoader.locateResource("shader/basic.frag", HelloWorld.class);
     Shader basicShader = new Shader(vert.toPath(), frag.toPath());
 
+    // Texture
+    PNGDecoder decoder =
+            new PNGDecoder(ResourceLoader.locateResource("images/grass.png", HelloWorld.class).toFileInputStream());
+    ByteBuffer buffer = ByteBuffer.allocateDirect(4 * decoder.getWidth() * decoder.getHeight());
+    decoder.decode(buffer, decoder.getWidth() * 4, PNGDecoder.Format.RGBA);
+    buffer.flip();
+
+    int texture = glGenTextures();
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, decoder.getWidth(), decoder.getHeight(), 0, GL_RGBA, GL_UNSIGNED_BYTE, buffer);
+    glGenerateMipmap(GL_TEXTURE_2D);
+
     Camera camera = new Camera(new Vector3f(0.0f, 0.0f, 5.0f));
 
     while (!context.isCloseRequested()) {
@@ -110,6 +129,9 @@ public class HelloWorld {
       Matrix4f view = camera.getViewMatrix();
       Matrix4f projection = new Matrix4f()
               .perspective((float) Math.toRadians(45.0f), 1080.0f / 720.0f, 0.01f, 1000.0f);
+
+      glActiveTexture(GL_TEXTURE0);
+      glBindTexture(GL_TEXTURE_2D, texture);
 
       basicShader.use();
       basicShader.setMatrix4("iModel", model);
--- src/main/resources/shader/basic.frag
+++ src/main/resources/shader/basic.frag
@@ -5,8 +5,9 @@ layout (location = 0) out vec4 FragColor;
 in vec2 Texs;
 
 uniform float iTime;
+uniform sampler2D sampler;
 
 void main() {
     //FragColor = vec4((sin(iTime / 2.0)+1.0)/2.0, (sin(iTime * 2.0)+1.0)/2.0, (sin(iTime)+1.0)/2.0, 1.0);
-    FragColor = vec4(Texs, Texs.x, 1.0);
+    FragColor = texture(sampler, Texs);
 }
