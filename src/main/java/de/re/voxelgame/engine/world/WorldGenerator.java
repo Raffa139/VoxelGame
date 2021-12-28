8ef28e74b3779bcfee04cac8fa19c5d58091e31c
Raffael E <raffa_e@web.de>
2021-12-28 16:03:42 +0100

    World generator notes

---
+++ src/main/java/de/re/voxelgame/engine/world/WorldGenerator.java
@@ -0,0 +1,9 @@
+package de.re.voxelgame.engine.world;
+
+public class WorldGenerator {
+  // Keeps track of chunks & their positions
+  // More advanced terrain generation => probably in other classes, but used in here
+  // Holds more advanced terrain generation parameters
+  // Loading chunks as needed (when camera moves)
+  // Memory management of chunk data
+}
