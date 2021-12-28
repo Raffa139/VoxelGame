403890b664dcb79abfd88d73b2898e2066880cc4
Raffael E <raffa_e@web.de>
2021-12-28 19:02:54 +0100

    Extend vector utility class

--- src/main/java/de/re/voxelgame/core/util/Vectors.java
+++ src/main/java/de/re/voxelgame/core/util/Vectors.java
@@ -6,9 +6,27 @@ public final class Vectors {
   private Vectors() {
   }
 
+  public static Vector3f mul(Vector3f v, Vector3f m) {
+    Vector3f res = new Vector3f();
+    v.mul(m, res);
+    return res;
+  }
+
   public static Vector3f mul(Vector3f v, float m) {
     Vector3f res = new Vector3f();
     v.mul(m, res);
+    return res;
+  }
+
+  public static Vector3f div(Vector3f v, Vector3f m) {
+    Vector3f res = new Vector3f();
+    v.div(m, res);
+    return res;
+  }
+
+  public static Vector3f div(Vector3f v, float m) {
+    Vector3f res = new Vector3f();
+    v.div(m, res);
     return res;
   }
 
@@ -18,9 +36,39 @@ public final class Vectors {
     return res;
   }
 
+  public static Vector3f add(Vector3f v, float m) {
+    Vector3f res = new Vector3f();
+    v.add(m, m, m, res);
+    return res;
+  }
+
+  public static Vector3f sub(Vector3f v, Vector3f m) {
+    Vector3f res = new Vector3f();
+    v.sub(m, res);
+    return res;
+  }
+
+  public static Vector3f sub(Vector3f v, float m) {
+    Vector3f res = new Vector3f();
+    v.sub(m, m, m, res);
+    return res;
+  }
+
   public static Vector3f cross(Vector3f v, Vector3f m) {
     Vector3f res = new Vector3f();
     v.cross(m, res);
+    return res;
+  }
+
+  public static Vector3f min(Vector3f v, Vector3f m) {
+    Vector3f res = new Vector3f();
+    v.min(m, res);
+    return res;
+  }
+
+  public static Vector3f max(Vector3f v, Vector3f m) {
+    Vector3f res = new Vector3f();
+    v.max(m, res);
     return res;
   }
 }
